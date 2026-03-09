1932ab282507b4da0cbffb19c4ba2474e263969e
jadenseantam <jadenseantam@gmail.com>
2026-03-09 21:54:08 +0800

    Initial Nuxt test commit

---
+++ test/app/pages/about.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="text-red-500">djaf;ksdjf;lkajsdfkl;ajs</h1>
+    <h1 class="text-red-600">Hello nuxt</h1>
+    <div class="h-20 w-20 bg-red-500 border-4 border-black">
+      If you see a red square, Tailwind is alive!
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ test/app/pages/docs.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Documentation</h1>
+    <p>This is the documentation page.</p>
+  </div>
+</template>
